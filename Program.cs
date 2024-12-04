94a442cc91c443d1db71ac18f9cc599f31c1b0c4
アルテム <143085970+artemtsvietkov@users.noreply.github.com>
2024-12-04 11:11:15 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,53 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        // Console.WriteLine("Enter your age!");
+        // int age = Convert.ToInt32(Console.ReadLine());
+
+        // if(age >= 0 && age <= 0) {
+        //     Console.WriteLine("White");
+
+        // }
+        // else if(age >= 13 && age <= 18) {
+        //     Console.WriteLine("Green");
+        // }
+        // else if(age >= 18 && age <= 25) {
+        //     Console.WriteLine("Red");
+        // }
+        // else if(age >= 25 && age <= 99) {
+        //     Console.WriteLine("Blue");
+        // }
+        // else{
+        //     Console.WriteLine("Invalid age.");
+        // }
+        
+        // Console.WriteLine("Enter a number: ");
+        // int number = Convert.ToInt32(Console.ReadLine());
+
+        // if(number%2==0) {
+        //     Console.WriteLine("The number is even");
+        // }
+        // else {
+        //     Console.WriteLine("the number is odd.");
+        // }
+
+        // Console.WriteLine("Enter a character: ");
+        // string? character = Console.ReadLine();
+
+        // switch(character) {
+        //     case "a":
+        //     case "e":
+        //     case "i":
+        //     case "o":
+        //     case "u":
+        //         Console.WriteLine("The character is vowel");
+        //         break;
+        //     default:
+        //         Console.WriteLine("The character is consonant");
+        //         break;
+
+        // }
+        
+    }
+}
